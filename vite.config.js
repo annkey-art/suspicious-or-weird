4e4c2194549d4d9e7c36fcf9c23b8d7c9026384c
annkey-art <annkey39@gmail.com>
2026-04-27 13:07:58 +0200

    Fix quotes in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/suspicious-or-weird/"
-});
+  base: '/suspicious-or-weird/'
+})
